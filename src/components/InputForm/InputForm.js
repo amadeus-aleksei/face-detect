9c21f0a2a0ff32623b0adb81ddc24a53dc7ee67e
Amadeus <amadeusaleksei@gmail.com>
2022-07-22 20:23:55 -0400

    Updated styling in TextFormInput.

--- src/components/InputForm/InputForm.js
+++ src/components/InputForm/InputForm.js
@@ -4,7 +4,8 @@ import './InputForm.css'
 const InputForm = () => {
     return(
         <div id='input-form' className='container'>
-            <h2>Input your image here to detect faces.</h2>
+            <h2>Detects the presence of a human face 
+                and locates it in a bounding box.</h2>
             <div className='container form-field'>
                 <input placeholder='Insert text here...'></input>
                 <button>Detect</button>
