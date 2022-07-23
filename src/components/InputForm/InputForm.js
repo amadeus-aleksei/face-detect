327496bab1329ae6cae447edb85d0a63b5c0cbe1
Amadeus <amadeusaleksei@gmail.com>
2022-07-23 02:56:58 -0400

    Changed text in InputForm.

--- src/components/InputForm/InputForm.js
+++ src/components/InputForm/InputForm.js
@@ -4,8 +4,8 @@ import './InputForm.css'
 const InputForm = () => {
     return(
         <div id='input-form' className='container'>
-            <h2>Detects the presence of a human face 
-                and locates it in a bounding box.</h2>
+            <h2>Detects the presence of human faces and 
+                envelops them with a bounding box.</h2>
             <div className='container form-field'>
                 <input placeholder='Insert text here...'></input>
                 <button>Detect</button>
